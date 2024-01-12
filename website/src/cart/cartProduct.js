1d1faddcf451df0c549eb428592f8cf3990c2e08
Mikosztyla <mikosztyla@gmai.com>
2024-01-12 18:06:22 +0100

    Working quantity

--- website/src/cart/cartProduct.js
+++ website/src/cart/cartProduct.js
@@ -1,24 +1,39 @@
+import React, { useState } from 'react';
 
+const CartProduct = () => {
+    const [quantity, setQuantity] = useState(1);
+    const [price, setPrice] = useState(299)
 
-export default function cartProduct(){
+    function increaseQuantity() {
+        setQuantity(prevQuantity => prevQuantity + 1);
+    }
+
+    function decreaseQuantity() {
+        if (quantity > 1) {
+            setQuantity(prevQuantity => prevQuantity - 1);
+        }
+    }
+
     return (
         <div className="cart_product_container">
             <div className="product_photo">
                 <img src='/media/cetusFPV.jpg'/>
             </div>
             <div className="single_price_container">
-                <p>299 zł</p>
+                <p>{price} zł</p>
             </div>
             <div className="quantity_container">
-                <p>1</p>
+                <p>{quantity}</p>
                 <div>
-                    <button>-</button>
-                    <button>+</button>
+                    <button onClick={decreaseQuantity}>-</button>
+                    <button onClick={increaseQuantity}>+</button>
                 </div>
             </div>
             <div className="overall_container">
-                <p>399 zł</p>
+                <p>{price * quantity} zł</p>
             </div>
         </div>
     )
 }
+
+export default CartProduct;
